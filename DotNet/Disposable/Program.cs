4c8b3a3ce57a36b8e3b7eb56123a916356067e6a
dmitriys <dmitriys@codevalue.net>
2017-07-24 16:15:39 +0300

    Added IDisposable

---
+++ DotNet/Disposable/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Disposable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dispose way.\r\n");
+
+            using (var resource2 = new MyClass())
+            {
+
+            }
+
+            Console.WriteLine("\r\n\r\nFinalizer way.\r\n");
+
+            var resource = new MyClass();
+            resource     = null;
+
+            GC.Collect();
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass : IDisposable
+    {
+        private bool disposed = false;
+
+        protected virtual void DisposeManaged()
+        {
+            // Free any managed resources here, e.g:
+            // this.resource.Dispose();
+
+            Console.WriteLine("Successfully disposed managed resources.");
+        }
+        protected virtual void DisposeUnmanaged()
+        {
+            // Free any unmanaged resources here, e.g:
+            // this.CloseHandle(this.handle);
+            // This code may be executed by finalizer, 
+            // so it MUST NOT reference any other objects,
+            // since those might already be collected.
+
+            Console.WriteLine("Successfully disposed unmanaged resources.");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                DisposeManaged();
+            }
+
+            DisposeUnmanaged();
+
+            disposed = true;
+        }
+
+        ~MyClass()
+        {
+            Console.WriteLine("Finalizer called.");
+            ReportFinalization();
+            Dispose(false);
+        }
+        
+        /// <summary>
+        /// Use this if you want the finalizer calls to be reported.
+        /// </summary>
+        protected virtual void ReportFinalization()
+        {
+            Console.WriteLine("Object finalization is most likely a bug.");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Boilerplate
+Contains code snippets for different tasks, components, and usages, which can be quickly combined and tweaked into a production quality application.
+
+## Project structure
+This project consists of many small projects that serve as a use case template divided by general themes.
+
+## Contribution
+Since Visual Studio solutions don't support physical folders, a workaround is used: the main solution contains a physical folder, that contains a theme solution. A logical folder with the same name is created in main solution, to where the theme solution is added.
